2d128f508c310325188e273c2b0b7dd20da7388d
Abdul Waleed <abdulwaleedfsd@gmail.com>
2025-02-09 14:02:36 -0800

    Added Header

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const App = () => {
-  return <div>app</div>;
+  return (
+    <div>
+      <h1>This is the first header.</h1>
+    </div>
+  );
 };
 
 export default App;
